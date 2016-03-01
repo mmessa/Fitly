f90da70013d521e477beabba66ab3a16ede4860c
mmessa <mmessa@mail.csuchico.edu>
2016-03-01 10:06:36 -0800

    adds Challenge and Item objects

---
+++ src/com/mattmessa/fitly/dao/Challenge.java
@@ -0,0 +1,13 @@
+package com.mattmessa.fitly.dao;
+
+public class Challenge {
+	
+	private int challengeId;
+	private String name;
+	private int level;
+	private String pictureUrl;
+	private String description;
+	private int coinsGiven;
+	private int experienceGiven;
+
+}
---
+++ src/com/mattmessa/fitly/dao/Item.java
@@ -0,0 +1,36 @@
+package com.mattmessa.fitly.dao;
+
+public class Item {
+
+		private int itemId;
+		private int price;
+		private String name;
+		private String imageUrl;
+		
+		public int getItemId() {
+			return itemId;
+		}
+		public void setItemId(int itemId) {
+			this.itemId = itemId;
+		}
+		public int getPrice() {
+			return price;
+		}
+		public void setPrice(int price) {
+			this.price = price;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getImageUrl() {
+			return imageUrl;
+		}
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+		
+		
+}
