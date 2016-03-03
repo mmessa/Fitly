ac11ae0ca73f39562beaac88d8cb3c90dec9e078
mmessa <mmessa@mail.csuchico.edu>
2016-03-03 12:30:53 -0800

    adds spring security dependencies. adds security filter. adds security config file

--- src/com/mattmessa/fitly/dao/BodyFat.java
+++ src/com/mattmessa/fitly/dao/BodyFat.java
@@ -1,5 +1,30 @@
 package com.mattmessa.fitly.dao;
 
+import java.util.Date;
+
 public class BodyFat {
 
+	private int userId;
+	private Date createdAtDate;
+	private double bodyFatPercentage;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public Date getCreatedAtDate() {
+		return createdAtDate;
+	}
+	public void setCreatedAtDate(Date createdAtDate) {
+		this.createdAtDate = createdAtDate;
+	}
+	public double getBodyFatPercentage() {
+		return bodyFatPercentage;
+	}
+	public void setBodyFatPercentage(double bodyFatPercentage) {
+		this.bodyFatPercentage = bodyFatPercentage;
+	}
+	
+	
 }
--- src/com/mattmessa/fitly/dao/Weight.java
+++ src/com/mattmessa/fitly/dao/Weight.java
@@ -1,5 +1,29 @@
 package com.mattmessa.fitly.dao;
 
-public class Weight {
+import java.util.Date;
 
+public class Weight {
+	private int userId;
+	private Date createdAtDate;
+	private double weight;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public Date getCreatedAtDate() {
+		return createdAtDate;
+	}
+	public void setCreatedAtDate(Date createdAtDate) {
+		this.createdAtDate = createdAtDate;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	
 }
