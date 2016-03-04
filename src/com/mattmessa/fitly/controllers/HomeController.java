4756ac35fdaa3008cab6dcd9bd03314abaf3411a
mmessa <mmessa@mail.csuchico.edu>
2016-03-04 09:35:50 -0800

    adds exception handler for database errors

--- src/com/mattmessa/fitly/controllers/HomeController.java
+++ src/com/mattmessa/fitly/controllers/HomeController.java
@@ -3,8 +3,10 @@ package com.mattmessa.fitly.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mattmessa.fitly.dao.User;
@@ -30,4 +32,11 @@ public class HomeController {
 			
 		return "home";
 	}
+	
+	@ExceptionHandler(DataAccessException.class)
+	public String handleDatabaseException(DataAccessException ex) {
+		
+		return "error";
+	}
+	
 }
